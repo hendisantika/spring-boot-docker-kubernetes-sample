e0910cf2ef5f1e2bf296d53024b2714b0a3fb5d0
hendisantika <hendisantika@yahoo.co.id>
2024-05-21 08:24:53 +0700

    Add createEmployee in EmployeeController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/k8s/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/k8s/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import id.my.hendisantika.k8s.entity.Employee;
 import id.my.hendisantika.k8s.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +33,11 @@ public class EmployeeController {
     @GetMapping("/all")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    // create employee rest api
+    @PostMapping("/create")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 }
