3542ab89bd5542466babf1facf02d57c70fc4b48
hendisantika <hendisantika@yahoo.co.id>
2024-05-21 08:20:11 +0700

    Add Employee Entity 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/k8s/entity/Employee.java
@@ -0,0 +1,43 @@
+package id.my.hendisantika.k8s.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-docker-kubernetes-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Visit: https://s.id/hendisantika
+ * Date: 21/05/24
+ * Time: 08.19
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+}
