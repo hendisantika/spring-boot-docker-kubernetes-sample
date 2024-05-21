63cc862a30ebcca78a2aadc37fc77d2a42927341
hendisantika <hendisantika@yahoo.co.id>
2024-05-21 08:25:03 +0700

    Add getEmployeeById in EmployeeController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/k8s/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/k8s/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package id.my.hendisantika.k8s.controller;
 
 import id.my.hendisantika.k8s.entity.Employee;
+import id.my.hendisantika.k8s.exception.ResourceNotFoundException;
 import id.my.hendisantika.k8s.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +42,13 @@ public class EmployeeController {
     @PostMapping("/create")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    // get employee by id rest api
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+        return ResponseEntity.ok(employee);
     }
 }
