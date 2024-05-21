c1e7d53792b98d78749797472ba39fea4f0bc8c7
hendisantika <hendisantika@yahoo.co.id>
2024-05-21 08:18:08 +0700

    Creat Spring Boot App 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/k8s/SpringBootDockerKubernetesSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerKubernetesSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerKubernetesSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/k8s/SpringBootDockerKubernetesSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerKubernetesSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
