c699284dd352873437832a35dc6db3773cfaad14
hendisantika <hendisantika@yahoo.co.id>
2024-05-24 08:17:44 +0700

    Add Testcontainer configs ✏️📗 :octocat: 🐳⬆

--- src/test/java/id/my/hendisantika/k8s/SpringBootDockerKubernetesSampleApplicationTests.java
+++ src/test/java/id/my/hendisantika/k8s/SpringBootDockerKubernetesSampleApplicationTests.java
@@ -1,13 +1,45 @@
 package id.my.hendisantika.k8s;
 
+import id.my.hendisantika.k8s.entity.Employee;
+import id.my.hendisantika.k8s.repository.EmployeeRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:mysql:8.4.0:///employeeDB"
+        },
+        webEnvironment = RANDOM_PORT
+)
 class SpringBootDockerKubernetesSampleApplicationTests {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @BeforeEach
+    void deleteAll() {
+        employeeRepository.deleteAll();
+    }
+
     @Test
-    void contextLoads() {
+    void createNewBook() {
+        Employee employee = new Employee();
+        employee.setId(1L);
+        employee.setFirstName("Itadori");
+        employee.setLastName("Yuji");
+        employee.setEmailId("yuji@yopmail.com");
+        employeeRepository.save(employee);
+
+        long count = employeeRepository.count();
+        Assertions.assertEquals(1, count);
     }
 
 }
